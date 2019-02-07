b8b7335ec7dd1454157bb592f38a5be5e5a69359
Owen Bellish <shadowguy224@live.com>
2019-02-07 13:51:14 -0500

    file path fix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const streamCat = (filePath) => {
     if (typeof filePath !== 'string') throw new TypeError(`File path must be a string, got ${typeof filePath}`);
-    console.log(path.join('..', '..', filePath));
+    console.log(path.join(__dirname, '..', '..', filePath));
 };
 
 module.exports = streamCat;
