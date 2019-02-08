d0e7e60333dd5c3c10c166eab2ec0ea31fe37280
Owen Bellish <shadowguy224@live.com>
2019-02-08 10:02:48 -0500

    work push

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const request = require('request-promise');
+const fetch = require('node-fetch');
 const fs = require('fs');
-const path = require('path');
-const streamCat = (filePath) => {
+const { join } = require('path');
+const { parse } = JSON;
+const streamCat = async (filePath) => {
     if (typeof filePath !== 'string') throw new TypeError(`File path must be a string, got ${typeof filePath}`);
-    console.log(path.join(__dirname, '..', '..', filePath));
+    const fullPath = join(__dirname, '..', '..', filePath);
+    fetch('https://aws.random.cat/meow').then(res => {
+        res.body.pipe(fullPath);
+    });
 };
 
 module.exports = streamCat;
