62caf929c3b4ebb8bf1489230f12f19c87481df8
Owen Bellish <shadowguy224@live.com>
2019-02-08 10:08:43 -0500

    removed temp image and fixed file stream

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const fetch = require('node-fetch');
 const fs = require('fs');
 const { join } = require('path');
 const { parse } = JSON;
-const streamCat = async (filePath) => {
+const streamCat = (filePath) => {
     if (typeof filePath !== 'string') throw new TypeError(`File path must be a string, got ${typeof filePath}`);
     const fullPath = join(__dirname, '..', '..', filePath);
     fetch('https://aws.random.cat/meow').then(res => {
-        res.body.pipe(fullPath);
+        const dest = fs.createWriteStream(fullPath);
+        res.body.pipe(dest);
     });
 };
 
